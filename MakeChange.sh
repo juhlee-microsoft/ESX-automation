646c89d364af5c9bad2a379616418dfaa09c41c4
Ju-Hyoung Lee <juhlee@microsoft.com>
2021-04-02 11:41:48 -0700

    fun script

---
+++ MakeChange.sh
@@ -0,0 +1,35 @@
+#
+# 1c, 10c, 25c
+#
+# MakeChange(10) == 1 ( 1 x 10c )
+# MakeChange(100) == 4 ( 4 x 25c )
+# MakeChange(11) == 2 ( 1x10c + 1x1c )
+# MakeChange(3) = 3
+# add all quo from each step
+
+function MakeChange() {
+    output=""
+    cents=$1
+    echo "Got $cents"
+    # if input is bigger than 25
+    if [[ $cents -ge 25 ]]; then
+        quo=$((cents / 25))
+        rem=$((cents % 25))
+        output=$quo
+        if [[ $rem -ge 10 ]]; then
+            quo=$((rem/10))
+            rem=$((rem%10))
+            output-$((output + $quo))
+        fi
+        output=$((output+$rem))
+    elif [[ $cents -lt 25 && $cents -ge 10 ]]; then
+        quo=$((cents / 10))
+        rem=$((cents % 10))
+        output=$((quo + $rem))
+    else
+    output=$cents
+    fi
+    echo $output
+}
+
+MakeChange $1
